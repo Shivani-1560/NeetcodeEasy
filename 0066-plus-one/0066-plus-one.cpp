7160dd94199a4c5ad5a6ab7b97f85c569602586b
Shivani-1560 <shivaniagarwal1215@gmail.com>
2026-03-26 00:59:28 +0530

    Time: 0 ms (100%), Space: 11.6 MB (26.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& a) {
+          long long n=a.size();
+         
+         for(int i=n-1;i>=0;i--){
+            if(a[i]<9){
+                a[i]++;
+                return a;
+            }
+       a[i]=0;
+            }
+            a.insert(a.begin(),1);
+            return a;
+        
+
+   
+
+
+
+
+      /*  for(int i=n-1;i>=0;i--){
+            if(i==n-1){
+            a[i]++;}
+           
+            if(a[i]==10){
+               a[i]=0;
+               if(i!=0){
+                a[i-1]++;
+                a[i-1]=0;
+               }
+             
+                 a.insert(a.begin(),1);
+            }
+               
+            }
+      return a;*/
+    
+    }
+};
+
