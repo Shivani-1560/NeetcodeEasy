6347c180b859a81b59b527da90e8bade0780037a
Shivani-1560 <shivaniagarwal1215@gmail.com>
2026-03-26 01:04:05 +0530

    Update 0021-merge-two-sorted-lists.cpp

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,31 +12,6 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
   
-        /*ListNode*temp1=list1;
-         ListNode*temp2=list2;
-        vector<int>arr;
-        while(temp1!=nullptr){
-            arr.push_back(temp1->val);
-            temp1=temp1->next;
-        }
-         while(temp2!=nullptr){
-            arr.push_back(temp2->val);
-            temp2=temp2->next;
-        }
-        sort(arr.begin(),arr.end());
-        ListNode*mergelist=nullptr;  //head of newlinkedlist 
-        ListNode*temp=nullptr;
-        for(int val:arr){
-        ListNode*newnode=new ListNode(val);
-        if(mergelist==nullptr){
-            mergelist=newnode;
-            temp=newnode;
-        }
-        else{
-           temp->next=newnode;
-            temp=temp->next;
-        }
-         } return mergelist;*/
    
    ListNode*dumynode=new ListNode(-1);
    ListNode*temp=dumynode;
